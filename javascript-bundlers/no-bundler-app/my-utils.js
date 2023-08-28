5ebfe640b5656ae91015c9a539c26b9d3e93fd50
Dawid Sibiński <dawidsibinski@gmail.com>
2023-08-28 07:17:23 +0400

    Split JS files

--- javascript-bundlers/no-bundler-app/my-utils.js
+++ javascript-bundlers/no-bundler-app/my-utils.js
@@ -1,11 +1,3 @@
 function doSomethingNice() {
   alert("Nice!");
 }
-
-function goToSecretPage() {
-  window.location.href = "secret.html";
-}
-
-function goToHomePage() {
-  window.location.href = "index.html";
-}
---
+++ javascript-bundlers/no-bundler-app/navigation.js
@@ -0,0 +1,7 @@
+function goToSecretPage() {
+  window.location.href = "secret.html";
+}
+
+function goToHomePage() {
+  window.location.href = "index.html";
+}
