4bdc7fe838d3220ec7b834e4c7be274f84cc2992
Dawid Sibiński <dawidsibinski@gmail.com>
2023-08-28 07:10:36 +0400

    Added no-bundler-app

---
+++ javascript-bundlers/no-bundler-app/my-utils.js
@@ -0,0 +1,11 @@
+function doSomethingNice() {
+  alert("Nice!");
+}
+
+function goToSecretPage() {
+  window.location.href = "secret.html";
+}
+
+function goToHomePage() {
+  window.location.href = "index.html";
+}
