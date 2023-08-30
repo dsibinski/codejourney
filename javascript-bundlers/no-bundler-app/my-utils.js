c9b082ceb7bcd2825f7b214dcce7d51f983c4bd6
Dawid Sibiński <dawidsibinski@gmail.com>
2023-08-30 07:32:29 +0400

    Uused functions added

--- javascript-bundlers/no-bundler-app/my-utils.js
+++ javascript-bundlers/no-bundler-app/my-utils.js
@@ -1,3 +1,15 @@
 export function doSomethingNice() {
   alert("Nice!");
 }
+
+export function notUsedFunctionA() {
+  alert("Not used function A");
+}
+
+export function notUsedFunctionB() {
+  alert("Not used function B");
+}
+
+export function notUsedFunctionC() {
+  alert("Not used function C");
+}
