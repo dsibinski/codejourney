c5610f8c63a5fa6835786a81e541d583df859438
Dawid Sibiński <dawidsibinski@gmail.com>
2023-09-02 11:21:04 +0400

    dist added for webpack app

---
+++ javascript-bundlers/webpack-app/dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";document.getElementById("reload-page-btn").addEventListener("click",(function(){window.location.reload()})),document.getElementById("say-sth-nice-btn").addEventListener("click",(function(){alert("Nice!")}))})();
