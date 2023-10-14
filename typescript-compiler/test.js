9065ff9c55bcbcb61e1db06c24701a9487d6bec6
Dawid Sibiński <dawidsibinski@gmail.com>
2023-10-14 17:02:07 +0400

    typescript-compiler: more complex example

--- typescript-compiler/test.js
+++ typescript-compiler/test.js
@@ -1,2 +1,16 @@
-"use strict";
-let age = 27;
+var Person = /** @class */ (function () {
+  function Person(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  Person.prototype.getName = function () {
+    return this.name;
+  };
+  Person.prototype.getAge = function () {
+    return this.age;
+  };
+  return Person;
+})();
+var person = new Person("John", 27);
+console.log(person.getName()); // Output: John
+console.log(person.getAge()); // Output: 27
--- typescript-compiler/test.ts
+++ typescript-compiler/test.ts
@@ -1,1 +1,21 @@
-let age: number = 27;
+class Person {
+  private name: string;
+  private age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public getAge(): number {
+    return this.age;
+  }
+}
+
+const person = new Person("John", 27);
+console.log(person.getName()); // Output: John
+console.log(person.getAge()); // Output: 27
